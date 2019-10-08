675911a0f375ad715dd13e8c1d8f4b741b8dbded
unknown <2422247006>
2019-10-08 13:05:11 +0800

    new file

---
+++ src/pages/manage.vue
@@ -0,0 +1,31 @@
+<template>
+  <div class="hello">
+    <!-- <van-button type="primary">主要按钮</van-button> -->
+    <div class="footer">
+      
+    </div>
+  </div>
+</template>
+
+<script>
+// import { Button } from "vant";
+export default {
+  name: "manage",
+  // components: {
+  //   [Button.name]: Button
+  // }
+};
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+.footer{
+  width:100%;
+  height:2rem;
+  background:red;
+  position: fixed;
+  bottom:0;
+  left:0;
+  right:0;
+}
+</style>
---
+++ src/router/index.js
@@ -0,0 +1,36 @@
+import Vue from 'vue'
+import Router from 'vue-router'
+import manage from '@/pages/manage'
+// import home from '@/pages/home'
+// import about from '@/pages/about'
+// import product from '@/pages/product'
+
+
+Vue.use(Router)
+
+export default new Router({
+    routes: [
+        {
+            path: '/',
+            component: manage,
+            name: 'manage',
+            // children: [
+            //     {
+            //     path: '',
+            //     component: home,
+            //     meta: [],
+            //     },
+            //     {
+            //         path: '/about',
+            //         component: about,
+            //         meta: [],
+            //     },
+            //     {
+            //         path: '/product',
+            //         component: product,
+            //         meta: [],
+            //         }
+            // ]
+        }
+    ]
+})
